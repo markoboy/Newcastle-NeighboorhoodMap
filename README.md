8caba050aabf6d00ae2c9e5dce2ad80e7e885095
Athanasios Markou <thanasismarko@gmail.com>
2018-08-10 17:12:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+https://sanderknape.com/2017/07/integrating-reactjs-google-maps-widget/
+
+# Resources
+
+- [Map Icon](http://freeiconshop.com/icon/location-map-icon-flat/)
+- [react-js-google-maps pckg](https://www.npmjs.com/package/react-js-google-maps)
+- [Unsplash API](https://unsplash.com/developers)
+- [Preload images](https://www.htmlgoodies.com/tutorials/web_graphics/article.php/3480001/So-You-Want-To-Pre-Load-Huh.htm)
+- [Foursquare API](https://developer.foursquare.com)
---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      zoom: 13,
+      maptype: 'roadmap',
+      map: null
+    }
+  }
+
+  componentDidMount() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: { lat: 54.978252, lng: -1.617780 },
+      // Hide the default google maps icons to prevent info window bug by opening
+      // an info window from the default locations. https://stackoverflow.com/questions/23390149/gmaps-api-remove-default-poi
+      styles: [
+        {
+          featureType: 'poi',
+          elementType: 'labels',
+          stylers: [{ visibility: 'off' }]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'labels',
+          stylers: [{ visibility: 'off'}]
+        }
+      ]
+    });
+
+    this.setState({ map });
+  }
+
+  render() {
+    // Store state variables for easier use.
+    const { map } = this.state;
+    return (
+      <div className="app" id='app'>
+        <header className="header"></header>
+        <div className="wrapper">
+          <aside className="sidebar"></aside>
+          <section className="map_container" tabIndex="-1">
+            <div id='map' className="map"></div>
+            {map ? '' : (<span>Error</span>)}
+          </section>
+        </div>
+      </div>
+    ); 
+  }
+}
+
+export default App;

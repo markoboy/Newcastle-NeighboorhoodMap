ed7cca69b6db7b8f94983d0ff36b779f4e9a9de1
Athanasios Markou <thanasismarko@gmail.com>
2018-08-11 21:14:52 +0100

    refactor: Move gMaps functions to be handled by a helper js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      gMapsHandler: true,
       zoom: 13,
       maptype: 'roadmap',
       map: null,
@@ -20,30 +21,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-      zoom: this.state.zoom,
-      maptype: this.state.maptype,
-      center: { lat: 54.978252, lng: -1.617780 },
-      // Hide the default google maps icons to prevent info window bug by opening
-      // an info window from the default locations. https://stackoverflow.com/questions/23390149/gmaps-api-remove-default-poi
-      styles: [
-        {
-          featureType: 'poi',
-          elementType: 'labels',
-          stylers: [{ visibility: 'off' }]
-        },
-        {
-          featureType: 'transit',
-          elementType: 'labels',
-          stylers: [{ visibility: 'off'}]
-        }
-      ]
-    });
-
-    // Create an info window google object to handle locations details.
-    let infoWindow = new window.google.maps.InfoWindow();
-
-    this.setState({ map, infoWindow });
+    // Initialize the Google Maps.
+    if (window.google)
+      gMapsHelper.initMap(this);
 
     // Get locations and locations data from Forsquare API.
     Locations.getLocations()
@@ -52,89 +32,22 @@ class App extends Component {
           .then(locationsData => this.setState({ locations, locationsData }));
 
         // Init the markers once data has been fetched.
-        this.initMarkers(map, locations);
+        if (window.google)
+          gMapsHelper.initMarkers(this, locations);
       });
   }
 
-  initMarkers = (map, locations) => {
-    const { google } = window;
-    // Store markers in an array.
-    let markers = [];
-    const bounds = new google.maps.LatLngBounds();
-
-    // Create the marker's highlighted icon and the default icon.
-    const highlightedIcon = this.createMarkerIcon('ee9999');
-    const defaultIcon = this.createMarkerIcon('0091ff');
-    this.setState({ markerIcons: { default: defaultIcon, highlighted: highlightedIcon }});
-
-    // Loop through locations to add them to the map.
-    locations.forEach( location => {
-      let position = {
-        lat: location.location.lat,
-        lng: location.location.lng
-      };
-      // Create the marker.
-      let marker = new google.maps.Marker({
-        title: location.name,
-        map,
-        position,
-        animation: google.maps.Animation.DROP,
-        icon: defaultIcon,
-        id: location.id
-      });
-
-      // Push marker to the array and extend the bounds.
-      markers.push(marker);
-      bounds.extend(marker.position);
-
-      // Add on click listener.
-      marker.addListener('click', () => this.handleMarkerOnClick(marker));
-
-      // Add on mouseover and mouseout listeners to change markers icons.
-      marker.addListener('mouseover', () => this.updateMarkerIcon(marker, highlightedIcon));
-      marker.addListener('mouseout', () => this.updateMarkerIcon(marker, defaultIcon));
-    });
-
-    // Fit the markers bounds to the map.
-    map.fitBounds(bounds);
-
-    this.state.infoWindow.addListener('closeclick', () => gMapsHelper.handleInfoWindowClosing(this.state.infoWindow, map, markers));
-
-    this.setState({ markers });
-  };
-
-  createMarkerIcon = (markerColor) => {
-    return new window.google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-      '|40|_|%E2%80%A2',
-      new window.google.maps.Size(25, 40),
-      new window.google.maps.Point(0, 0),
-      new window.google.maps.Point(10, 34),
-      new window.google.maps.Size(25, 40)
-    );
-  };
-
-  updateMarkerIcon = (marker, markerIcon) => marker.setIcon(markerIcon);
+  updateMarkerIcon = (marker, markerIcon) => {
+    this.setState(state => ({
+      markers: state.markers.map(m => {
+        if (m === marker) m.setIcon(markerIcon);
+        return m;
+      })
+    }));
+  }
 
   handleMarkerOnClick = (marker) => {
-    const { map, infoWindow, locationsData } = this.state;
-    // Check to make sure InfoWindow is not opened with this marker.
-    if ( infoWindow.marker !== marker ) {
-      // Add an animation to the marker.
-      marker.setAnimation(null);
-      marker.setAnimation(window.google.maps.Animation.BOUNCE);
-      setTimeout(() => marker.setAnimation(null), 1000); // Stop animation after 1second.
-
-      // Center the map and zoom in when infowindow opens.
-      let center = { lat: marker.position.lat() + 0.005, lng: marker.position.lng() };
-      map.setCenter(center);
-      map.setZoom(15);
-
-      infoWindow.marker = marker;
-      let locationInfo = locationsData.filter( location => location.id === marker.id)[0]; // Get the location info based on the marker id.
-      infoWindow.setContent(`<b>${locationInfo.name}</b>`);
-      infoWindow.open(map, marker);
-    }
+    gMapsHelper.handleMarkerOnClick(this, marker);
   }
 
   render() {
--- src/utils/GoogleMapsHelper.js
+++ src/utils/GoogleMapsHelper.js
@@ -3,7 +3,130 @@
  * This is a helper file to organize the google maps
  * functions to add to the markers and the infowindow.
  */
-export const handleInfoWindowClosing = (infoWindow, map, markers) => {
+export const initMap = (app) => {
+	if (!app.state.gMapsHandler) return console.log('GoogleMapsHelper.js - initMap: wrong argument passed. Please pass App component as an argument.');
+
+	let map = new window.google.maps.Map(document.getElementById('map'), {
+	  zoom: app.state.zoom,
+	  maptype: app.state.maptype,
+	  center: { lat: 54.978252, lng: -1.617780 },
+	  // Hide the default google maps icons to prevent info window bug by opening
+	  // an info window from the default locations. https://stackoverflow.com/questions/23390149/gmaps-api-remove-default-poi
+	  styles: [
+	    {
+	      featureType: 'poi',
+	      elementType: 'labels',
+	      stylers: [{ visibility: 'off' }]
+	    },
+	    {
+	      featureType: 'transit',
+	      elementType: 'labels',
+	      stylers: [{ visibility: 'off'}]
+	    }
+	  ]
+	});
+
+	// Create an info window google object to handle locations details.
+	let infoWindow = new window.google.maps.InfoWindow();
+
+	app.setState({ map, infoWindow });
+};
+
+export const initMarkers = (app, locations) => {
+	if (!app.state.gMapsHandler) return console.log('GoogleMapsHelper.js - initMarkers: wrong argument passed. Please pass App component as an argument.');
+
+  const { google } = window;
+  const { map, infoWindow } = app.state;
+  // Store markers in an array.
+  let markers = [];
+  const bounds = new google.maps.LatLngBounds();
+
+  // Create the marker's highlighted icon and the default icon.
+  const highlightedIcon = createMarkerIcon('ee9999');
+  const defaultIcon = createMarkerIcon('0091ff');
+
+  // Loop through locations to add them to the map.
+  locations.forEach( location => {
+    let position = {
+      lat: location.location.lat,
+      lng: location.location.lng
+    };
+    // Create the marker.
+    let marker = new google.maps.Marker({
+      title: location.name,
+      map,
+      position,
+      animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
+      id: location.id
+    });
+
+    // Push marker to the array and extend the bounds.
+    markers.push(marker);
+    bounds.extend(marker.position);
+
+    // Add on click listener.
+    marker.addListener('click', () => app.handleMarkerOnClick(marker));
+
+    // Add on mouseover and mouseout listeners to change markers icons.
+    marker.addListener('mouseover', () => app.updateMarkerIcon(marker, highlightedIcon));
+    marker.addListener('mouseout', () => app.updateMarkerIcon(marker, defaultIcon));
+  });
+
+  // Fit the markers bounds to the map.
+  map.fitBounds(bounds);
+
+  infoWindow.addListener('closeclick', () => handleInfoWindowClosing(app));
+
+  app.setState({ map, markers, markerIcons: { default: defaultIcon, highlighted: highlightedIcon }});
+};
+
+const createMarkerIcon = (markerColor) => {
+  return new window.google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new window.google.maps.Size(25, 40),
+    new window.google.maps.Point(0, 0),
+    new window.google.maps.Point(10, 34),
+    new window.google.maps.Size(25, 40)
+  );
+};
+
+export const handleMarkerOnClick = (app, marker) => {
+	if (!app.state.gMapsHandler) return console.log('GoogleMapsHelper.js - handleMarkerOnClick: wrong argument passed. Please pass App component as an argument.');
+
+  const { map, infoWindow, locationsData } = app.state;
+  // Check to make sure InfoWindow is not opened with this marker.
+  if ( infoWindow.marker !== marker ) {
+    app.setState(state => ({
+      markers: state.markers.map(m => {
+        if (m === marker) {
+          // Add an animation to the marker.
+          m.setAnimation(null);
+          m.setAnimation(window.google.maps.Animation.BOUNCE);
+          setTimeout(() => m.setAnimation(null), 1000); // Stop animation after 1second.
+        }
+        return m;
+      })
+    }));
+
+    // Center the map and zoom in when infowindow opens.
+    let center = { lat: marker.position.lat() + 0.005, lng: marker.position.lng() };
+    map.setCenter(center);
+    map.setZoom(15);
+
+    infoWindow.marker = marker;
+    let locationInfo = locationsData.filter( location => location.id === marker.id)[0]; // Get the location info based on the marker id.
+    infoWindow.setContent(`<b>${locationInfo.name}</b>`);
+    infoWindow.open(map, marker);
+    app.setState({ map, infoWindow });
+  }
+};
+
+export const handleInfoWindowClosing = (app) => {
+	if (!app.state.gMapsHandler) return console.log('GoogleMapsHelper.js - handleInfoWindowClosing: wrong argument passed. Please pass App component as an argument.');
+
+	const { infoWindow, markers, map } = app.state;
 	// Close the info window and empty the previous marker.
 	infoWindow.close();
 	infoWindow.marker = null;
@@ -13,4 +136,5 @@ export const handleInfoWindowClosing = (infoWindow, map, markers) => {
 	markers.filter( marker => marker.map !== null ).map( m => bounds.extend(m.position));
 	map.fitBounds(bounds);
 	map.setZoom(14);
+	app.setState({ infoWindow, map });
 };

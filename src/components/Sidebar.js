4b5294b7e188bcf26928a4ee1b22fd9145158b7f
Athanasios Markou <thanasismarko@gmail.com>
2018-08-11 11:32:04 +0100

    feat: Filter locations by a search input

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,51 @@ import React, { Component } from 'react';
 import LocationsList from './LocationsList';
 
 class Sidebar extends Component {
+	constructor() {
+		super();
+		this.state = {
+			query: ''
+		};
+	}
+
+	updateQuery = (query) => {
+		this.setState({ query: query.replace(/\s\s+/g, ' ') });
+	};
+
+	clearQuery = () => {
+		this.setState({ query: '' });
+
+		// Focus the search bar.
+		document.querySelector('.filter-bar input').focus();
+	};
+
+	/* Handle the keypress on the clear-query button */
+	handleKeyDown = e => {
+		let allowedKeys = {
+			'32': 'Space',
+			'13': 'Enter'
+		};
+		// If a key from the allowed keys is pressed clear query.
+		if (allowedKeys[e.keyCode]) {
+			e.preventDefault();
+			this.clearQuery();
+		}
+	};
+
+	/* Handle the submit of the query */
+	handleSubmit = e => {
+		e.preventDefault();
+
+		// Set focus to the first button child of the locations list.
+		let locationsList = document.querySelector('.locations_list');
+		if (locationsList.firstChild) {
+			locationsList.firstChild.firstChild.focus();
+		} else {
+			// Set the focus on the results to alert user that there were no results.
+			document.querySelector('#results').focus();
+		}
+	};
+
 	// Find the marker from the location id and update its color on button hover.
 	updateMarker = (location, markerIcon) => {
 		const { markers, markerIcons, updateMarkerIcon } = this.props;
@@ -27,6 +72,14 @@ class Sidebar extends Component {
 
 	render() {
 		const { locations } = this.props;
+		const { query } = this.state;
+
+		let showingLocations;
+		if (query) {
+			showingLocations = locations.filter( location => location.name.toLowerCase().includes(query.toLowerCase()) );
+		} else {
+			showingLocations = locations;
+		}
 
 		return (
 			<aside
@@ -34,16 +87,37 @@ class Sidebar extends Component {
 				className="sidebar"
 				aria-labelledby="menubutton">
 				<div className="filter-bar">
-					<form onSubmit={(e) => e.preventDefault()}>
+					<form onSubmit={(e) => this.handleSubmit(e)}>
 						<input
 							type="search"
 							placeholder="Search locations"
+							value={query}
+							onChange={(event) => this.updateQuery(event.target.value)}
 						/>
+						{query ? (
+							<div className="clear-query">
+								{showingLocations.length > 0 ?
+									<span id="results">Showing locations for `{query}`.</span> :
+									<span id="results" tabIndex="-1">No results where found.</span>
+								}
+								<div
+									className="clear-query_button"
+									tabIndex="0"
+									role="button"
+									onClick={() => this.clearQuery()}
+									onKeyDown={(e) => this.handleKeyDown(e)}
+								>Clear query</div>
+							</div>
+						) : (
+							<div className="clear-query">
+								<span id="results">Showing all locations.</span>
+							</div>
+						)}
 					</form>
 				</div>
 				<div>
 					<LocationsList
-						locations={locations}
+						locations={showingLocations}
 						updateIcon={this.updateMarker}
 						handleClick={this.openInfoWindow}
 					/>

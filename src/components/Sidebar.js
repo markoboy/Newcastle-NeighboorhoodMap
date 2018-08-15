ca86f0bbf89864421adaea9549ebf4a863e50ee9
Athanasios Markou <thanasismarko@gmail.com>
2018-08-15 22:00:00 +0100

    feat: Add background images on sidebar from Unsplash API

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,80 @@ import React, { Component } from 'react';
 import LocationsList from './LocationsList';
 
 class Sidebar extends Component {
+	constructor() {
+		super();
+		this.state = {
+			images: [],
+			imageUrl: '',
+			imageInd: 0,
+			reqCounter: 0
+		};
+	};
+
+	componentDidMount() {
+		// Check if images data are stored on local storage.
+		if (window.localStorage.newcastleImages) {
+			const images = JSON.parse(window.localStorage.getItem('newcastleImages'));
+			this.setState({ images });
+		} else {
+			this.getImages();
+		}
+
+		this.updateBackgroundImage();
+	}
+
+	// Get images data from Unsplash API.
+	getImages = () => {
+		let headers = new Headers();
+		headers.append('Authorization', 'Client-ID b34d1a4835eee3492a28ef2557dce130b515531438d5f100b60c5bfc2daeac5a');
+
+		fetch('https://api.unsplash.com/search/photos?page=1&query=Newcastle+upon+Tyne', { headers })
+			.then(response => {
+				if (!response.ok) {
+					return response.json()
+						.then(error => this.props.handleError(error, 'Failed to get background images from Unsplash API.'));
+				} else if (response.ok) {
+					return response.json()
+						.then(data => {
+							this.setState({ images: data.results });
+
+							window.localStorage.setItem('newcastleImages', JSON.stringify(data.results));
+						});
+				}
+			}).catch(error => this.props.handleError(error, 'An error occured while getting the background images.'));
+	};
+
+	// Update backgrounds images every 20s.
+	updateBackgroundImage = () => {
+		const { images, imageInd, reqCounter} = this.state;
+		let timer;
+
+		// Check if the images have received data from the Unsplash API.
+		if (images.length > 0) {
+			// Preload the image before setting it, in order to have smooth transition between the changes.
+			let preloadImage = new Image();
+			preloadImage.src = images[imageInd].urls.regular;
+			preloadImage.onload = () => this.setState({ imageUrl: preloadImage.src });
+
+			timer = setTimeout(() => {
+				// Check if the index is smaller than the images array.
+				images.length - 1 > imageInd ?
+					this.setState(state => ({ imageInd: state.imageInd + 1 })) : // If so increase the index value.
+					this.setState({ imageInd: 0 }); // Else start over from 0 index.
+				this.updateBackgroundImage();
+			}, 20000);
+		} else if (reqCounter < 10) { // Else check again in a while.
+			timer = setTimeout(() => {
+				// Set a timer request counter in case that there is no online conection.
+				this.setState(state => ({ reqCounter: state.reqCounter++ }));
+				this.updateBackgroundImage();
+			}, 500);
+		} else {
+			// If more than 20 requests have been made stop requests.
+			clearTimeout(timer);
+		}
+	};
+
 	/* Handle the keypress on the clear-query button */
 	handleKeyDown = e => {
 		let allowedKeys = {
@@ -78,7 +152,9 @@ class Sidebar extends Component {
 			<aside
 				id="sidebar"
 				className={isOpened ? "sidebar open" : "sidebar"}
-				style={isOpened ? {display: 'inherit'} : {display: ''}}
+				style={isOpened ?
+					(this.state.imageUrl ? {display: 'inherit', backgroundImage: `url(${this.state.imageUrl}`} : {display: 'inherit'})
+					: {display: ''}}
 				aria-labelledby="menubutton"
 				onKeyDown={(e) => this.handleEscapeKey(e)}>
 				<div className="filter-bar">
